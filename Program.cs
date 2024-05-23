ec2e5766b8dde1b49f5f3c9c6d9546c9136cfeca
Made By IToncek <itoncek3@itoncek.space>
2024-05-23 22:43:53 +0200

    Removing unnecessary files

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace ktane_irl
+﻿using System;
+using System.Device.I2c;
+
+namespace ktane_irl
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello I2C!");
+            I2cDevice i2c = I2cDevice.Create(new I2cConnectionSettings(1, 0x12));
+            i2c.WriteByte(0x42);
+            var read = i2c.ReadByte();
+            Console.WriteLine(read);
         }
     }
 }
