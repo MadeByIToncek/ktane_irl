ad4b2fae504950317dd3828b40721d86b0ff2d1e
Made By IToncek <itoncek3@itoncek.space>
2024-05-26 20:02:36 +0200

    Network init

--- src/main/java/space/itoncek/ktane_irl/KtaneIRL.java
+++ src/main/java/space/itoncek/ktane_irl/KtaneIRL.java
@@ -11,19 +11,32 @@
  *                 Made by IToncek & JellyCZ
  */
 
+/*
+ *
+ * ██╗  ██╗████████╗ █████╗ ███╗   ██╗███████╗    ██╗██████╗ ██╗
+ * ██║ ██╔╝╚══██╔══╝██╔══██╗████╗  ██║██╔════╝    ██║██╔══██╗██║
+ * █████╔╝    ██║   ███████║██╔██╗ ██║█████╗      ██║██████╔╝██║
+ * ██╔═██╗    ██║   ██╔══██║██║╚██╗██║██╔══╝      ██║██╔══██╗██║
+ * ██║  ██╗   ██║   ██║  ██║██║ ╚████║███████╗    ██║██║  ██║███████╗
+ * ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝    ╚═╝╚═╝  ╚═╝╚══════╝
+ *
+ *            Licensed under terms in LICENSE.md
+ *                 Made by IToncek & JellyCZ
+ */
+
 package space.itoncek.ktane_irl;
 
-import com.pi4j.boardinfo.util.BoardInfoHelper;
 import com.pi4j.util.Console;
+import io.javalin.Javalin;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.StringJoiner;
+import java.io.File;
+import java.io.IOException;
 
 public class KtaneIRL {
 	public static final String prefix = ";";
 	private static final I2CLib lib = new I2CLib();
 	private static final Console log = new Console();
+	private static Javalin app;
 
 	public static void main(String[] args) {
 		System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "INFO");
@@ -31,59 +44,43 @@ public class KtaneIRL {
 
 		Runtime.getRuntime().addShutdownHook(new Thread(KtaneIRL::shutdown));
 
-		setupInputScanner();
+		setupNetworkHandler();
 	}
 
-	private static void setupInputScanner() {
-		Scanner sc = new Scanner(System.in);
-		while (sc.hasNextLine()) {
-			String line = sc.nextLine();
-			CommandParser.Commands parse = CommandParser.parse(line);
-			if (parse != null) {
-				switch (parse) {
-					case HELP -> printHelp();
-					case STATUS -> printStatus();
-					case FORCEGC -> runGC();
-					case SAFE_EXIT, EXIT -> System.exit(0);
-				}
+	private static void setupNetworkHandler() {
+		app = Javalin.create(config -> {
+			config.http.gzipOnlyCompression(9);
+			config.jetty.defaultPort = 7777;
+			File tempfolder;
+			try {
+				tempfolder = File.createTempFile("tempDir", "ktaneIRL");
+				tempfolder.mkdirs();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
 			}
-		}
-	}
+			config.jetty.multipartConfig.cacheDirectory(tempfolder.getAbsolutePath());
 
-	private static void printHelp() {
-		ArrayList<String> list = new ArrayList<>();
-		for (CommandParser.Commands value : CommandParser.Commands.values()) {
-			list.add(prefix + value.commands[0] + " --> " + value.description);
-			list.add("\tAlternatives: " + getAlternatives(value.commands));
-			list.add("\t");
-		}
-		String[] array = list.toArray(new String[0]);
-		log.box(array);
-	}
+			config.requestLogger.http((ctx, ms) -> {
+				log.print(ctx.path() + " in " + ms);
+			});
 
-	private static String getAlternatives(String[] commands) {
-		StringJoiner js = new StringJoiner(", ");
-		for (String command : commands) {
-			js.add(prefix + command);
-		}
-		return js.toString();
+			config.requestLogger.ws(ws -> ws.onMessage(ctx -> System.out.println("Received: " + ctx.message())));
+
+			config.router.ignoreTrailingSlashes = true; // treat '/path' and '/path/' as the same path
+			config.router.treatMultipleSlashesAsSingleSlash = true; // treat '/path//subpath' and '/path/subpath' as the same path
+			config.router.caseInsensitiveRoutes = true; // treat '/PATH' and '/path' as the same path
+		});
+
+
+		app.start();
 	}
 
 	public static void shutdown() {
 		lib.close();
+		app.stop();
 	}
 
 	private static void init() {
-		// ------------------------------------------------------------
-		// Output Pi4J Board information
-		// ------------------------------------------------------------
-		// When the Pi4J Context is initialized, a board detection is
-		// performed. You can use this info in case you need board-specific
-		// functionality.
-		// OPTIONAL
-
-		printStatus();
-
 		log.box("                                                                  ",
 				"██╗  ██╗████████╗ █████╗ ███╗   ██╗███████╗    ██╗██████╗ ██╗     ",
 				"██║ ██╔╝╚══██╔══╝██╔══██╗████╗  ██║██╔════╝    ██║██╔══██╗██║     ",
@@ -93,14 +90,6 @@ public class KtaneIRL {
 				"╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝    ╚═╝╚═╝  ╚═╝╚══════╝",
 				"                                                                  ",
 				"                Running your games since 2024!");
-	}
-
-	private static void printStatus() {
-		log.box("Board model: " + BoardInfoHelper.current().getBoardModel().getLabel(),
-				"Raspberry Pi model with RP1 chip (Raspberry Pi 5): " + BoardInfoHelper.usesRP1(),
-				"OS is 64-bit: " + BoardInfoHelper.is64bit(),
-				"JVM memory used (MB): " + BoardInfoHelper.getJvmMemory().getUsedInMb(),
-				"Board temperature (°C): " + BoardInfoHelper.getBoardReading().getTemperatureInCelsius());
 	}
 
 	public static void runGC() {
