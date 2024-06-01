1f252ad3af42ccf0bf6f7c13e96579e9a805e28e
Daniel Hejduk <danielhejduk@disroot.org>
2024-06-01 11:57:01 +0200

    Adding status blink header library

--- src/i2c2i2c/main.c
+++ src/i2c2i2c/main.c
@@ -1,18 +1,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include "status.h"
 
 int main(void) {
     // Set pin 0 (PB0) and pin 1 (PB1) as output
     DDRB |= (1 << DDB0) | (1 << DDB1);
 
-    while (1) {
-        // Turn the LEDs on
-        PORTB |= (1 << PORTB0) | (1 << PORTB1);
-        _delay_ms(250);
+    enum BLINKSTATUS status = NORMAL;
 
-        // Turn the LEDs off
-        PORTB &= ~((1 << PORTB0) | (1 << PORTB1));
-        _delay_ms(250);
+    while (1) {
+        status_blink(status);
     }
     
     return 0; // This line will never be reached
