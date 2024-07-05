a6e5d3d79d11f5885055c76a549082f76cf13b40
Daniel Hejduk <danielhejduk@disroot.org>
2024-07-05 10:51:26 +0200

    Adding basic i2c library

--- src/i2c2i2c/src/main.rs
+++ src/i2c2i2c/src/main.rs
@@ -1,14 +1,23 @@
 #![no_std]
 #![no_main]
+
 #![feature(asm_experimental_arch)]
+
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_mut)]
 
 use core::panic::PanicInfo;
 
 use attiny_hal::Peripherals;
-use avr_hal_generic::hal::digital::v2::InputPin;
-use delay::{delay_ms, delay_us};
+use delay::delay_ms;
+use i2c::i2c_send;
+use i2c::I2cConnection;
 
 mod delay;
+mod i2c;
+
+use bit_struct::{u7, u1};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -21,15 +30,12 @@ fn main() -> ! {
     let pins = attiny_hal::pins!(peripherals);
 
     let mut led = pins.pb1.into_output();
-    let mut sda = pins.pb0.into_floating_input();
-    let mut scl = pins.pb2.into_floating_input();
+    let mut i2cin: i2c::I2cInput = i2c::I2cInput{sda: pins.pb0.into_floating_input(), scl: pins.pb2.into_floating_input()};
+    let mut i2cout: i2c::I2cOutput = i2c::I2cOutput{sda: pins.pb3.into_output_high(), scl: pins.pb4.into_output_high()};
+
+    i2c_send(&mut i2cout, &I2cConnection::new(u7!(0b0001001), u1!(1)));
 
     loop {
-        if (sda.is_low()) {
-            led.set_high();
-        } else {
-                led.toggle();
-                delay_ms(1000);
-            }
-        }
+        delay_ms(10)
     }
+}
